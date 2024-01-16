1e9c21e1ceaab02adc23ec615da496ae5c255fbb
nicholaskim7 <154484441+nicholaskim7@users.noreply.github.com>
2024-01-16 17:19:24 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var number = 10;
+var string ='helloworld';
+var isRad = true;
+
+if(number == 10){
+    console.log('yeah');
+}
+else{
+    console.log('nope');
+}
+document.getElementById('box').innerHTML = number + 5;
